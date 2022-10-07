b38aa27287b96255fd480f2b24f48ba087e75b1b
wonth93 <wongth93@gmail.com>
2022-10-07 03:27:46 +0000

    update css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,46 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
+
+$(document).ready(function() {
+
+  const createTweetElement = function(dataObj) {
+    const $tweet = `<article class="tweet">
+    ${dataObj.user.name}
+    </article>`
+    return $tweet;
+  }
+  
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+  
+  const $tweet = createTweetElement(tweetData);
+  $('#tweet-container').append($tweet);
+  
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+console.log('connect!!')
